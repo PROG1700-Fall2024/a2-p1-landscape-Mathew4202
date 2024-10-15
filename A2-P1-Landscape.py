5d1380068873cb1cb5c045d5ce42c60498a31dae
Matew4202 <w0461513@campus.nscc.ca>
2024-10-15 18:03:07 -0300

    Program 1 done

--- A2-P1-Landscape.py
+++ A2-P1-Landscape.py
@@ -2,17 +2,54 @@
 #A landscaping company needs a program that computes the price of landscaping for a new housing development. 
 
 #Student #:     
-#Student Name:  
+#Student Name:  Mathew Akunyili
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
+    #Declare variables
+    labour = 1000
+  
 
+    #Title
+    print("Landscape Calculator")
+    print("")
 
+    #Ask user for house number
+    address = input("Enter House Number: ")
+    print("")
 
+    #Ask for proberty length and width
+    length = float(input("Enter property length(feet): "))
+    width = float(input("Enter property width(feet): "))
 
+    #Ask user for type of grass
+    grass = input("Enter type of grass (fescue, bentgrass, campus): ").lower()
 
+    #Ask user for number of trees
+    trees = int(input("Enter the number of trees: "))
 
+    #calculatuons
+    squareFeet = length * width
+
+    if grass == "fescue":
+        grassPrice = 0.05 * squareFeet
+    elif grass == "bentgrass":
+        grassPrice = 0.02 * squareFeet
+    elif grass == "campus":
+        grassPrice = 0.01 * squareFeet
+
+    if squareFeet > 5000:
+        upCharge = 500
+    else: 
+        upCharge = 0
+
+    totalCost = grassPrice + (trees * 100) + labour + upCharge
+        
+    
+    print("Total cost for house", address, f"is: ${totalCost:.2f}")
+
+    
 
     # YOUR CODE ENDS HERE
 
